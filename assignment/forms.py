c667fb0bb7594ea5a975b5cb961aabd627e3f65e
chuk <chuk@localhost.localdomain>
2020-02-11 14:52:47 -0600

    minor adjustments to form

--- assignment/forms.py
+++ assignment/forms.py
@@ -12,7 +12,8 @@ TRICKS = (('item_key1', 'Sit'),
 class DogForm(forms.Form):
 	daily_walk = forms.BooleanField(required=False, label="Do you walk your dog daily? ")
 	dog_breed = forms.CharField(required=True, label="What breed is your dog? ")
-	dog_age = forms.IntegerField(required=True, label="How old is your dog? ")
+	dog_age = forms.IntegerField(required=True, label="How old is your dog? ", max_value=30, 
+		min_value=0)
 	dog_tricks = forms.MultipleChoiceField(label="What tricks does your dog know? Select all that apply: ", 
 		choices=TRICKS)
 	email = forms.CharField(required=True, label="Email Address: ")
